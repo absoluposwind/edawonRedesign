b45ce265f33b6074409257e5c2ff5a7ea66c40cb
absoluposwind <46222263+absoluposwind@users.noreply.github.com>
2022-07-20 12:51:53 +0900

    Add files via upload

--- js/redesign_product.js
+++ js/redesign_product.js
@@ -1,3 +1,43 @@
+document.documentElement.addEventListener('touchstart', function (event) {
+    if (event.touches.length > 1) {
+         event.preventDefault(); 
+       } 
+   }, false);
+
+var lastTouchEnd = 0; 
+
+document.documentElement.addEventListener('touchend', function (event) {
+    var now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+         event.preventDefault(); 
+       } lastTouchEnd = now; 
+   }, false);
+
 $("document").ready(function(){
+
+    const $nav_list = $(".header .nav .nav-wrap ul li");
     $(".content2 .item-about ul li input").eq(0).addClass("on")
+
+    
+    $($nav_list).mouseover(function(){
+        $(this).find("a").css("color", "#00b33f");
+        $(this).find(".sub-menu ul li a").css("color", "black");
+    }).mouseout(function(){
+        $(this).find("a").css("color", "");
+    })
+
+    $(".header .nav .nav-wrap ul li .sub-menu ul li").mouseover(function(){
+        $(this).css("background-color", "#e7e7e7");
+    }).mouseout(function(){
+        $(this).css("background-color", "");
+    })
+
+    $(window).scroll(function(){
+        let pos = $(window).scrollTop();
+        if(pos >= 202){
+            $(".nav").addClass("on")
+        }else{
+            $(".nav").removeClass("on")
+        }
+    })
 })
